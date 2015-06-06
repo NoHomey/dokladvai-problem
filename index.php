307bb584fcb7eb88d9dd1f0cc97d3a502a189240
martog <martin.holy.grigorov@gmail.com>
2015-06-06 11:55:44 +0300

    updating index.php

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
 		
 		<body>
 			<form align = "center">
+				<p>Подател</p>
 				<input type = "text"placeholder = "име" id = "fname" /><br>
 				<input type = "text"placeholder = "фамилия" id = "lname" style = "margin-top:2px;" /><br>
-				<input type = "text"placeholder = "ел.поща" id = "email" style = "margin-top:2px;" />
+				<input type = "text"placeholder = "ел.поща" id = "email" style = "margin-top:2px;" /><br>
+				<input type = "text"placeholder = "телефон" id = "number" style = "margin-top:2px;" /><br>
+				
+				<p>Сигнал</p>
+				<input type = "text"placeholder = "локация" id = "location"  /><br>
+				<input type = "text"placeholder = "описание" id = "description" style = "margin-top:2px;" /><br>
 			</form>
 		</body>
 		
