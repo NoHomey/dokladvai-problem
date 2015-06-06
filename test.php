072899fcf076f159fb5330dd75410184be21a292
martog <martin.holy.grigorov@gmail.com>
2015-06-06 18:07:26 +0300

    update all

--- test.php
+++ test.php
@@ -1,16 +1,30 @@
-
+<meta charset = "UTF-8">
 
 <table>
+<tr>
+<td>Номер</td>
+<td>Име</td>
+<td>Фамилия</td>
+<td>Ел. поща</td>
+<td>Тел. номер</td>
+<td>Дата</td>
+<td>Локация</td>
+<td>Описание</td>
+<td>До институция</td>
+<td>Снимка</td>
+</tr>
 <?php
-	require("config.php")
-	$result = mysql_query("SELECT * FROM signal");
-	while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+	require("config.php");
+	$query = "SELECT * FROM  `signal` "; 
+	$result = mysql_query($query);
+	while($row = mysql_fetch_row($result)){ 
 		echo "<tr>";
-		for($i = 0; $i < 8;++i) {
-	    	echo "<td>" . $row[0] . "</td>";  
+		for($i = 0; $i < 10;++$i) {
+	    	echo "<td>" . $row[$i] . "</td>";  
 	    }
-	   	echo "<td>" . "<img src = '".  $row[0] . "'>". "</td>";  
-	    echo "</tr>"; 
+	   	echo "<td> <a href = ' $row[9]'><img src = ' $row[9]' height = 50px width = 50px /> </a> </td>";
+	    echo "</tr>";
 	}
+	echo $result;
 ?>
 </table>
