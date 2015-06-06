fe74502793801057a0e2e52e4640d01cd3b3ebcf
martog <martin.holy.grigorov@gmail.com>
2015-06-06 11:42:16 +0300

    updating index.php

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <html>
 		<head>
+			<title>Home</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		</head>
 		
 		<body>
-			<form>
+			<form align = "center">
+				<input type = "text"placeholder = "име" id = "fname" /><br>
+				<input type = "text"placeholder = "фамилия" id = "lname" style = "margin-top:2px;" /><br>
+				<input type = "text"placeholder = "ел.поща" id = "email" style = "margin-top:2px;" />
 			</form>
 		</body>
 		
 		
 </html>
+
+<?php
+		mysql_connect("localhost", "root", "");
+		mysql_select_db("signal");
+?>
+
+
